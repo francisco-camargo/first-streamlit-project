4c361385963a73eff42271e2a8c4f82446c618ba
Francisco Camargo <86134108+francisco-camargo@users.noreply.github.com>
2024-01-07 21:58:23 -0600

    Basic streamlit usage

--- main.py
+++ main.py
@@ -2,6 +2,44 @@
 Run in terminal with
     streamlit run main.py
 '''
+import altair as alt
+import numpy as np
+import pandas as pd
 import streamlit as st
 
-st.write('Hello World!')
+st.header('my st.button')
+
+if st.button('Say hello'):
+    st.write('Why hello there')
+else:
+    st.write('Goodbye')
+
+# Ex 1
+st.write('Hello *World!* :sunglasses:')
+
+# Ex 2
+st.write(1234)
+
+# Ex 3
+df = pd.DataFrame(
+    {
+        'first col': [1, 23, 4, 2],
+        'second col': [10, 40, 2, 1],
+    }
+)
+st.write(df)
+
+# Ex 4
+st.write('below is df', df, 'above is df')
+
+# Ex 5
+df2 = pd.DataFrame(
+    np.random.randn(200, 3),
+    columns=['a', 'b', 'c'],
+)
+chart = (
+    alt.Chart(df2)
+    .mark_circle()
+    .encode(x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+)
+st.write(chart)
